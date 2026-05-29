67fd4b53fda93974814fd9c6c7c1cb6df4a05254
João Pinheiro <lucasdallas25@gmail.com>
2026-05-29 20:26:29 -0300

    chore: initial Scaffold

---
+++ src/test/java/br/com/marazulturismo/marazulbackendadmin/MarazulBackEndAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marazulturismo.marazulbackendadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarazulBackEndAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
