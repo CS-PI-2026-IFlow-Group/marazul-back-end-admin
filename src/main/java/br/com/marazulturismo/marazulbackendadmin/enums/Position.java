b352ebff96d3cb6b4d7d2fbdf5b0d0ced3b2be53
João Pinheiro <lucasdallas25@gmail.com>
2026-05-29 22:30:00 -0300

    Implements the user model with defined roles and positions.

---
+++ src/main/java/br/com/marazulturismo/marazulbackendadmin/enums/Position.java
@@ -0,0 +1,4 @@
+package br.com.marazulturismo.marazulbackendadmin.enums;
+public enum Position {
+DRIVER
+}
---
+++ src/main/java/br/com/marazulturismo/marazulbackendadmin/enums/UserRole.java
@@ -0,0 +1,7 @@
+package br.com.marazulturismo.marazulbackendadmin.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER,
+    NO_ACCESS
+}
